f7d60127e8e7cc3e72ab8169cda81d54f317edb9
Zakarum <scareaangel@gmail.com>
2018-06-13 14:51:46 +0300

    Use custom cast

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,3 @@
-use hal::memory::{cast_slice, Pod};
 use std::borrow::Cow;
 
 pub fn is_slice_sorted<T: Ord>(slice: &[T]) -> bool {
@@ -19,22 +18,34 @@ pub fn is_slice_sorted_by_key<'a, T, K: Ord, F: Fn(&'a T) -> K>(slice: &'a [T], 
     true
 }
 
-pub fn cast_vec<A: Pod, B: Pod>(mut vec: Vec<A>) -> Vec<B> {
+pub fn cast_vec<T>(mut vec: Vec<T>) -> Vec<u8> {
     use std::mem;
 
-    let raw_len = mem::size_of::<A>() * vec.len();
-    let len = raw_len / mem::size_of::<B>();
+    let raw_len = mem::size_of::<T>() * vec.len();
+    let len = raw_len;
 
-    let raw_cap = mem::size_of::<A>() * vec.capacity();
-    let cap = raw_cap / mem::size_of::<B>();
-    assert_eq!(raw_cap, mem::size_of::<B>() * cap);
+    let cap = mem::size_of::<T>() * vec.capacity();
 
     let ptr = vec.as_mut_ptr();
     mem::forget(vec);
     unsafe { Vec::from_raw_parts(ptr as _, len, cap) }
 }
 
-pub fn cast_cow<A: Pod, B: Pod>(cow: Cow<[A]>) -> Cow<[B]> {
+pub fn cast_slice<T>(slice: &[T]) -> &[u8] {
+    use std::{mem, slice::from_raw_parts};
+
+    let raw_len = mem::size_of::<T>() * slice.len();
+    let len = raw_len;
+
+    let ptr = slice.as_ptr();
+    mem::forget(slice);
+    unsafe { from_raw_parts(ptr as _, len) }
+}
+
+pub fn cast_cow<T>(cow: Cow<[T]>) -> Cow<[u8]>
+where
+    T: Clone,
+{
     match cow {
         Cow::Borrowed(slice) => Cow::Borrowed(cast_slice(slice)),
         Cow::Owned(vec) => Cow::Owned(cast_vec(vec)),
